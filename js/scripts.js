02dcebc343546198aec1aff273ebfa4faa254298
Rudi Kovac <nasal@outlook.com>
2021-09-14 13:25:11 +0200

    add class to "active" menu item on page scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
+const header = document.querySelector('header')
 const links = document.querySelectorAll('.menu li a');
+const news = document.getElementById('block-adchallenge-content')
+const events = document.getElementById('block-latest-event')
+const about = document.getElementById('block-about-me')
+const menuOffset = header.offsetHeight
+let scrollPos = window.scrollY;
+
+(function() {
+  links[0].classList.add('active');
+})();
 
 for (const link of links) {
   link.addEventListener('click', clickHandler);
@@ -9,14 +19,37 @@ function clickHandler(e) {
   const href = this.getAttribute('href');
   const offsetTop = document.querySelector(href).offsetTop;
 
-  for (const link of links) {
-    link.classList.remove('active')
-  }
-
-  e.target.classList.add('active')
-
   scroll({
     top: offsetTop,
-    behavior: 'smooth'
+    behavior: 'smooth',
   });
 }
+
+function activateOnScroll(el) {
+  el.classList.add('active');
+}
+
+function deactivateOnScroll() {
+  for (const link of links) {
+    link.classList.remove('active');
+  }
+}
+
+window.addEventListener('scroll', function() {
+  const [homeMenu, newsMenu, eventMenu, aboutMenu] = links;
+  scrollPos = window.scrollY;
+  deactivateOnScroll();
+
+  if (scrollPos <= news.offsetTop - menuOffset) {
+    activateOnScroll(homeMenu);
+  }
+  else if (scrollPos >= news.offsetTop - menuOffset && scrollPos <= events.offsetTop - menuOffset) {
+    activateOnScroll(newsMenu);
+  }
+  else if (scrollPos >= events.offsetTop - menuOffset && scrollPos <= about.offsetTop - menuOffset) {
+    activateOnScroll(eventMenu);
+  }
+  else {
+    activateOnScroll(aboutMenu);
+  }
+});
